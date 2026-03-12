9643bf8fba64e6ccc11dd1a041f8376333a8b3b9
Nithin <nithinrontala>
2026-03-12 13:40:47 +0530

    vite change to base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nithin-portfolio/', // Set base path for deployment
   plugins: [react()],
 });
