a0729e5dc2addcd6efa16f0e90a15d9ae829b7ec
Nithin <nithinrontala>
2026-03-12 13:14:58 +0530

    final

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -26,54 +26,60 @@ export default function setSplitText() {
       para.split?.revert();
     }
 
-    para.split = new SplitText(para, {
-      type: "lines,words",
-      linesClass: "split-line",
-    });
+    // Fix: Check if para is defined before calling .split
+    if (para) {
+      para.split = new SplitText(para, {
+        type: "lines,words",
+        linesClass: "split-line",
+      });
 
-    para.anim = gsap.fromTo(
-      para.split.words,
-      { autoAlpha: 0, y: 80 },
-      {
-        autoAlpha: 1,
-        scrollTrigger: {
-          trigger: para.parentElement?.parentElement,
-          toggleActions: ToggleAction,
-          start: TriggerStart,
-        },
-        duration: 1,
-        ease: "power3.out",
-        y: 0,
-        stagger: 0.02,
-      }
-    );
+      para.anim = gsap.fromTo(
+        para.split.words,
+        { autoAlpha: 0, y: 80 },
+        {
+          autoAlpha: 1,
+          scrollTrigger: {
+            trigger: para.parentElement?.parentElement,
+            toggleActions: ToggleAction,
+            start: TriggerStart,
+          },
+          duration: 1,
+          ease: "power3.out",
+          y: 0,
+          stagger: 0.02,
+        }
+      );
+    }
   });
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
-    title.split = new SplitText(title, {
-      type: "chars,lines",
-      linesClass: "split-line",
-    });
-    title.anim = gsap.fromTo(
-      title.split.chars,
-      { autoAlpha: 0, y: 80, rotate: 10 },
-      {
-        autoAlpha: 1,
-        scrollTrigger: {
-          trigger: title.parentElement?.parentElement,
-          toggleActions: ToggleAction,
-          start: TriggerStart,
-        },
-        duration: 0.8,
-        ease: "power2.inOut",
-        y: 0,
-        rotate: 0,
-        stagger: 0.03,
-      }
-    );
+    // Fix: Check if title is defined before calling .split
+    if (title) {
+      title.split = new SplitText(title, {
+        type: "chars,lines",
+        linesClass: "split-line",
+      });
+      title.anim = gsap.fromTo(
+        title.split.chars,
+        { autoAlpha: 0, y: 80, rotate: 10 },
+        {
+          autoAlpha: 1,
+          scrollTrigger: {
+            trigger: title.parentElement?.parentElement,
+            toggleActions: ToggleAction,
+            start: TriggerStart,
+          },
+          duration: 0.8,
+          ease: "power2.inOut",
+          y: 0,
+          rotate: 0,
+          stagger: 0.03,
+        }
+      );
+    }
   });
 
   ScrollTrigger.addEventListener("refresh", () => setSplitText());
